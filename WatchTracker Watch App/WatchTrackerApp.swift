0520d925bd9311de3adf6566e843854d92b64389
30108586 <30108585@students.southwales.ac.uk>
2026-02-18 10:26:11 +0000

    Initial Commit

---
+++ WatchTracker Watch App/WatchTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTrackerApp.swift
+//  WatchTracker Watch App
+//
+//  Created by Ejimofor I J (FCES) on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WatchTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
